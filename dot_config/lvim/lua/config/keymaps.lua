4949ce2a2cd26ffd9c5f0d8fbc6f6aaf5279da20
Kevin Chien <kchien@easypost.com>
2024-09-25 17:27:03 +0000

    enable extras: copilot, copilot-chat, lang.go, lang.markdown; remove s mappings, config smartyank

--- dot_config/lvim/lua/config/keymaps.lua
+++ dot_config/lvim/lua/config/keymaps.lua
@@ -2,13 +2,9 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
--- -- c goes to black hole register
+-- c goes to black hole register
 vim.keymap.set("n", "c", '"_c', { noremap = true })
 vim.keymap.set("v", "c", '"_c', { noremap = true })
-
--- s goes to black hole register
-vim.keymap.set("n", "s", '"_s', { noremap = true })
-vim.keymap.set("v", "s", '"_s', { noremap = true })
 
 -- x goes to black hole register
 vim.keymap.set("n", "x", '"_x', { noremap = true })
