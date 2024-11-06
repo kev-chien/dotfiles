3f9968eedd72c1520ad514eab9606778e5c25bef
Kevin Chien <kchien@easypost.com>
2024-11-06 23:12:45 +0000

    nvim: move colorscheme to oxocarbon, golang no gofumpt, manually specify mason lsp servers, rm python extra, add neotest extra

---
+++ dot_config/nvim/lua/plugins/colors.lua
@@ -0,0 +1,34 @@
+return {
+  {
+    "kev-chien/nord.nvim",
+  },
+  {
+    "nyoom-engineering/oxocarbon.nvim",
+  },
+  {
+    "rebelot/kanagawa.nvim",
+    opts = {
+      colors = {
+        theme = {
+          all = {
+            ui = {
+              bg_gutter = "none"
+            }
+          }
+        }
+      }
+    }
+  },
+  {
+    "projekt0n/github-nvim-theme",
+  },
+  {
+    "EdenEast/nightfox.nvim",
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "oxocarbon",
+    },
+  },
+}
---
+++ dot_config/nvim/lua/plugins/mason.lua
@@ -0,0 +1,25 @@
+return {
+  "williamboman/mason.nvim",
+  opts = {
+    ensure_installed = {
+      "goimports",
+      "cmakelang",
+      "cmakelint",
+      "docker-compose-language-service",
+      "dockerfile-language-server",
+      "goimports",
+      "gopls",
+      "hadolint",
+      "json-lsp",
+      "lua-language-server",
+      "markdown-toc",
+      "markdownlint-cli2",
+      "marksman",
+      "pyright",
+      "shfmt",
+      "sqlfluff",
+      "stylua",
+      "yaml-language-server",
+    }
+  },
+}
