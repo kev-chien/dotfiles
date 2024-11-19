12bbad96435094488fe0bf9de900df663ef68d24
Kevin Chien <kchien@easypost.com>
2024-11-19 04:01:01 +0000

    .config/nvim: add yankassassin, update lazyvim to 7

---
+++ dot_config/nvim/lua/plugins/yankassassin.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "svban/YankAssassin.nvim",
+    config = function()
+      require("YankAssassin").setup({
+        auto_normal = true, -- if true, autocmds are used. Whenever y is used in normal mode, the cursor doesn't move to start
+        auto_visual = true, -- if true, autocmds are used. Whenever y is used in visual mode, the cursor doesn't move to start
+      })
+    end,
+  },
+}
