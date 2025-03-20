81181de6ae72deeb3b71597c51b7a4b9db3f10ee
Kevin Chien <kchien@easypost.com>
2025-03-20 23:00:19 +0000

    nvimrc: improve highlight for todo-comments, don't highlight surrounding characters

--- dot_config/nvim/lua/plugins/todo_comments.lua
+++ dot_config/nvim/lua/plugins/todo_comments.lua
@@ -3,6 +3,8 @@ return {
     "folke/todo-comments.nvim",
     opts = {
       highlight = {
+        -- highlight only the matched characteres, not surrounding characters (which is default behavior)
+        keyword = "bg",
         -- allow matching without the colon (which is in defaults)
         pattern = [[.*<(KEYWORDS)\s*]],
       },
